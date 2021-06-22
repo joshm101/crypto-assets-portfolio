02fcbfb5ef4d598f20bc730eba9863574062bb05
Joshua Mora <jshm101@gmail.com>
2021-06-21 19:21:46 -0700

    feat: exchange select component
    Adds exchange select UI component + unit tests to verify functionality

---
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -0,0 +1,43 @@
+import { FunctionComponent } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { Exchange } from '../../types';
+
+interface ExchangeSelectProps {
+  onChange: () => {};
+  value: string;
+  exchanges: Exchange[];
+}
+
+const ExchangeSelect: FunctionComponent<ExchangeSelectProps> = ({
+  onChange,
+  value,
+  exchanges,
+}) => {
+  return (
+    <FormControl>
+      <InputLabel>Exchange</InputLabel>
+      <Select
+        onChange={onChange}
+        value={value}
+        name="exchangeId"
+        data-test-id="exchange-select"
+      >
+        {exchanges.map((exchange) => (
+          <MenuItem key={exchange.id} value={exchange.id}>
+            {exchange.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+const TransactionForm = {
+  ExchangeSelect,
+};
+
+export default TransactionForm;
